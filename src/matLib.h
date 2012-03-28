89b92e46ec57c79bcd4aa22fa55f9f24c35d0ac1
Ben Jones <ben.james.jones@gmail.com>
2012-03-28 15:19:44 -0600

    SVD testing on host

--- src/matLib.h
+++ src/matLib.h
@@ -23,6 +23,17 @@ struct mat3{
 
 struct mat2{
   float m00, m01, m10, m11;
+};
+
+
+inline void printVector(const vec3& v ){
+  std::cout << v.x << ' ' << v.y << ' ' <<v.z << std::endl;
+}
+
+inline void printMatrix(const mat3& m){
+  std::cout << m.m00 << ' ' << m.m01 << ' ' << m.m02 << '\n' <<
+    m.m10 << ' ' << m.m11 << ' ' << m.m12 << '\n' <<
+    m.m20 << ' ' << m.m21 << ' ' << m.m22 << std::endl;
 }
   
 
@@ -38,15 +49,179 @@ struct mat2{
   
   }*/
 
-__device__ void SVD(const mat3& A,
+__host__ /*__device__*/ void SVD(const mat3& A,
 		    mat3& U,
 		    vec3& S,
-		    mat3& V);
+		    mat3& V){
 
+  //see http://www.math.pitt.edu/~sussmanm/2071Spring08/lab09/index.html
 
-inline void printVector(const vec3& v ){
-  std::cout << v.x << ' ' << v.y << ' ' <<v.z << std::endl;
+  const float tol = 1.e-6;
+
+  U = A; //copy it over to start with
+  V.m00 = 1;
+  V.m01 = 0;
+  V.m02 = 0;
+  V.m10 = 0;
+  V.m11 = 1;
+  V.m12 = 0;
+  V.m20 = 0;
+  V.m21 = 0;
+  V.m22 = 1; //V starts as identity;
+
+  std::cout << "A: ";
+  printMatrix(A);
+  std::cout << "U: " ;
+  printMatrix(U);
+  std::cout << "V: ";
+  printMatrix(V);
+
+  float converge = 1.0f + tol;
+
+  while (converge > tol){
+
+    converge = 0.0f;
+    //do for (i, j) pairs:
+    // (0,1), (0,2), (1,2)
+    float alpha, beta, gamma;
+    float zeta, t, c, s;
+    //0,1 pair, i = 0, j = 1
+    
+    alpha = U.m00*U.m00 + U.m10*U.m10 + U.m20*U.m20;
+    beta =  U.m01*U.m01 + U.m11*U.m11 + U.m21*U.m21;
+    gamma = U.m00*U.m01 + U.m10*U.m11 + U.m20*U.m21;
+
+    if(gamma){
+
+      converge = max(converge, abs(gamma)*rsqrt(alpha*beta));
+
+      zeta = (beta - alpha)/(2*gamma);
+      t = signum(zeta)/(abs(zeta) + sqrtf(1 + zeta*zeta));
+      c = rsqrtf(1 + t*t); //1/sqrt(1 + t^2)
+      s = c*t;
+      
+      std::cout << alpha << ' ' << beta << ' ' << gamma << ' ' << converge << ' ' << zeta << ' ' << t << ' ' << c << std::endl;
+      
+      //update cols i,j of U:
+      t = U.m00;
+      U.m00 = c*t - s*U.m01;
+      U.m01 = s*t + c*U.m01;
+      t = U.m10;
+      U.m10 = c*t - s*U.m11;
+      U.m11 = s*t + c*U.m11;
+      t = U.m20;
+      U.m20 = c*t - s*U.m21;
+      U.m21 = s*t + c*U.m21;
+      
+      //update V:
+      t = V.m00;
+      V.m00 = c*t - s*V.m01;
+      V.m01 = s*t + c*V.m01;
+      t = V.m10;
+      V.m10 = c*t - s*V.m11;
+      V.m11 = s*t + c*V.m11;
+      t = V.m20;
+      V.m20 = c*t - s*V.m21;
+      V.m21 = s*t + c*V.m21;
+      
+      printMatrix(U);
+      std::cout << "v: ";
+      printMatrix(V);
+    }
+    //i = 0, j = 2
+    alpha = U.m00*U.m00 + U.m10*U.m10 + U.m20*U.m20;
+    beta =  U.m02*U.m02 + U.m12*U.m12 + U.m22*U.m22;
+    gamma = U.m00*U.m02 + U.m10*U.m12 + U.m20*U.m22;
+
+    if(gamma){
+      converge = max(converge, abs(gamma)*rsqrt(alpha*beta));
+      
+      zeta = (beta - alpha)/(2*gamma);
+      t = signum(zeta)/(abs(zeta) + sqrtf(1 + zeta*zeta));
+      c = rsqrtf(1 + t*t); //1/sqrt(1 + t^2)
+      s = c*t;
+      
+      //update cols i,j of U:
+      t = U.m00;
+      U.m00 = c*t - s*U.m02;
+      U.m02 = s*t + c*U.m02;
+      t = U.m10;
+      U.m10 = c*t - s*U.m12;
+      U.m12 = s*t + c*U.m12;
+      t = U.m20;
+      U.m20 = c*t - s*U.m21;
+      U.m21 = s*t + c*U.m21;
+      
+      //update V:
+      t = V.m00;
+      V.m00 = c*t - s*V.m02;
+      V.m02 = s*t + c*V.m02;
+      t = V.m10;
+      V.m10 = c*t - s*V.m12;
+      V.m12 = s*t + c*V.m12;
+      t = V.m20;
+      V.m20 = c*t - s*V.m22;
+      V.m22 = s*t + c*V.m22;
+    }
+    //i = 1, j = 2
+    alpha = U.m01*U.m01 + U.m11*U.m11 + U.m21*U.m21;
+    beta =  U.m02*U.m02 + U.m12*U.m12 + U.m22*U.m22;
+    gamma = U.m01*U.m02 + U.m11*U.m12 + U.m21*U.m22;
+    
+    if(gamma){
+      converge = max(converge, abs(gamma)*rsqrt(alpha*beta));
+      
+      zeta = (beta - alpha)/(2*gamma);
+      t = signum(zeta)/(abs(zeta) + sqrtf(1 + zeta*zeta));
+      c = rsqrtf(1 + t*t); //1/sqrt(1 + t^2)
+      s = c*t;
+      
+      //update cols i,j of U:
+      t = U.m01;
+      U.m01 = c*t - s*U.m02;
+      U.m02 = s*t + c*U.m02;
+      t = U.m11;
+      U.m11 = c*t - s*U.m12;
+      U.m12 = s*t + c*U.m12;
+      t = U.m21;
+      U.m21 = c*t - s*U.m21;
+      U.m21 = s*t + c*U.m21;
+      
+      //update V:
+      t = V.m01;
+      V.m01 = c*t - s*V.m02;
+      V.m02 = s*t + c*V.m02;
+      t = V.m11;
+      V.m11 = c*t - s*V.m12;
+      V.m12 = s*t + c*V.m12;
+      t = V.m21;
+      V.m21 = c*t - s*V.m22;
+      V.m22 = s*t + c*V.m22;
+    }
+    
+    
+  }
+
+  //sing vals = column norms of U
+  S.x = sqrtf(U.m00*U.m00 + U.m10*U.m10 + U.m20*U.m20);
+  S.y = sqrtf(U.m01*U.m01 + U.m11*U.m11 + U.m21*U.m21);
+  S.z = sqrtf(U.m02*U.m02 + U.m12*U.m12 + U.m22*U.m22);
+  
+  U.m00 /= S.x;
+  U.m10 /= S.x;
+  U.m20 /= S.x;
+
+  U.m01 /= S.y;
+  U.m11 /= S.y;
+  U.m21 /= S.y;
+
+  U.m02 /= S.z;
+  U.m12 /= S.z;
+  U.m22 /= S.z;
+  
 }
+
+
 
 
 #endif
--- src/testSVD.cu
+++ src/testSVD.cu
@@ -1,12 +1,9 @@
 
 #include "matLib.h"
 
-void __global__ svdKernel(const mat3& A, mat3* U, vec3* S, mat3* V){
+void __global__ svdKernel(mat3 *A, mat3* U, vec3* S, mat3* V){
 
-
-  SVD(A, *U, *S, *V);
-
-  
+  //SVD(*A, *U, *S, *V); 
 
 }
 
@@ -14,7 +11,7 @@ void __global__ svdKernel(const mat3& A, mat3* U, vec3* S, mat3* V){
 int main(int argc, char ** argv){
 
 
-  mat3 A_h = {1,0,0, 0, 1, 0, 0, 0, 1};
+  mat3 A_h = {1,1,0, 0, 20, 0, 0, 0, 1};
 
   mat3 U_h, V_h;
   vec3 S_h;
@@ -22,7 +19,7 @@ int main(int argc, char ** argv){
   mat3 *A_d, *U_d, *V_d;
   vec3 *S_d;
 
-  cudaMalloc(&A_d, sizeof(mat3));
+  /*cudaMalloc(&A_d, sizeof(mat3));
   cudaMalloc(&U_d, sizeof(mat3));
   cudaMalloc(&V_d, sizeof(mat3));
   cudaMalloc(&S_d, sizeof(vec3));
@@ -31,11 +28,26 @@ int main(int argc, char ** argv){
 
   svdKernel<<<1,1>>>(A_d, U_d, S_d, V_d);
 
+  cudaThreadSynchronize();
+
   cudaMemcpy(&U_h, U_d, sizeof(mat3), cudaMemcpyDeviceToHost);
   cudaMemcpy(&V_h, V_d, sizeof(mat3), cudaMemcpyDeviceToHost);
   cudaMemcpy(&S_h, S_d, sizeof(vec3), cudaMemcpyDeviceToHost);
 
+  printVector(S_h);*/
+
+
+  //host version:
+  SVD(A_h, U_h, S_h, V_h);
+  
+  std::cout << "output: ";
+  printMatrix(U_h);
+
+  std::cout << "s";
   printVector(S_h);
+
+  std::cout << "v";
+  printMatrix(V_h);
 
   return 0;
 }
